ecfaf51e4566de5cae14f9654a6248c4c990edf0
ShaoxiongYuan <58580328+ShaoxiongYuan@users.noreply.github.com>
2020-02-21 19:28:50 +0800

    d13

--- 2. Python高级软件开发技术/4. MySQL/Day13/exercise_insert.sql
+++ 2. Python高级软件开发技术/4. MySQL/Day13/exercise_insert.sql
@@ -4,4 +4,8 @@ values (1, '骆驼祥子', '老舍', '中国人民出版社', 100, '值得阅读
        (2, '平凡的世界', '路遥', '清华大学出版社', 90, '非常好');
 insert into book
 values (3, '活着', '余华', '商务印书馆', 100, '值得深思');
+insert into book(name, author, publisher, price, note)
+values ('朝花夕拾', '鲁迅', '人民大学出版社', 50, '棒');
+
+
 
