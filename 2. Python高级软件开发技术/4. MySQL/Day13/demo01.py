f2d2f3702dd686d98bfdb08facdd38ad54755a7d
ShaoxiongYuan <58580328+ShaoxiongYuan@users.noreply.github.com>
2020-02-21 18:03:18 +0800

    d13

--- 2. Python高级软件开发技术/4. MySQL/Day13/demo01.py
+++
@@ -1,3 +0,0 @@
-import pymysql
-
-pymysql.connect()
---
+++ 2. Python高级软件开发技术/4. MySQL/Day13/my_sql.sql
@@ -0,0 +1,12 @@
+use student;
+insert into class_1
+values (1, 'Baron', 10, 'm', 91),
+       (2, 'Jame', 9, 'm', 90),
+       (3, 'Lily', 19, 'w', 100);
+insert into class_1 (name, age, sex, score)
+VALUES ('Ammy', 20, 'w', 100);
+insert into interest
+values (1, 'Emma', 'sing,dance', 'A', 1680.00, '骨骼精奇，练舞奇才'),
+       (2, 'Lily', 'dance', 'B', 9900, 'good');
+show databases;
+
