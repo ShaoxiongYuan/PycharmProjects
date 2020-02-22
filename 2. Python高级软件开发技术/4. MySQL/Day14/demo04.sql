f8217b640425ec942f0e401acc02651772d8f5e8
ShaoxiongYuan <58580328+ShaoxiongYuan@users.noreply.github.com>
2020-02-22 17:59:03 +0800

    d14

--- 2. Python高级软件开发技术/4. MySQL/Day14/demo04.sql
+++ 2. Python高级软件开发技术/4. MySQL/Day14/demo04.sql
@@ -4,3 +4,35 @@ from sanguo;
 
 select avg(attack)
 from sanguo;
+
+select sum(attack)
+from sanguo;
+
+select min(attack)
+from sanguo;
+
+select count(name)
+from sanguo;
+
+select count(*)
+from sanguo
+where country = '蜀';
+
+select gender, count(*)
+from sanguo
+group by gender;
+
+select country, avg(attack)
+from sanguo
+group by country;
+
+select country, gender, count(*)
+from sanguo
+group by country, gender;
+
+select country, count(*) as number
+from sanguo
+where gender = '男'
+group by country
+order by number desc
+limit 2;
