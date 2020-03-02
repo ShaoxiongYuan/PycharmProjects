b769510c9fbc7f146f76b07c1b8334a08a2566d1
ShaoxiongYuan <58580328+ShaoxiongYuan@users.noreply.github.com>
2020-03-02 11:01:52 +0800

    d11

--- 2. Python高级软件开发技术/2. IO/Day11/http_server_demo.py
+++ 2. Python高级软件开发技术/2. IO/Day11/http_server_demo.py
@@ -6,12 +6,13 @@ io多路服用，类封装练习
 from socket import *
 from select import *
 
+
 # 具体功能实现
 class HTTPServer:
-    def __init__(self,host='0.0.0.0',port=8000,dir=None):
+    def __init__(self, host='0.0.0.0', port=8000, dir=None):
         self.host = host
         self.port = port
-        self.address = (host,port)
+        self.address = (host, port)
         self.dir = dir
         # 多路服用监控列表
         self.rlist = []
@@ -24,41 +25,38 @@ class HTTPServer:
     # 启动服务
     def serve_forever(self):
         self.sockfd.listen(3)
-        print("Listen the port %d"%self.port)
+        print("Listen the port %d" % self.port)
         # 使用IO多路服用处理客户端请求
         self.rlist.append(self.sockfd)
         while True:
-            rs,ws,xs = select(self.rlist,
-                              self.wlist,
-                              self.xlist)
+            rs, ws, xs = select(self.rlist,
+                                self.wlist,
+                                self.xlist)
             for r in rs:
                 if r is self.sockfd:
-                    c,addr = r.accept()
+                    c, addr = r.accept()
                     c.setblocking(False)
-                    self.rlist.append(c) # 增加监控对象
+                    self.rlist.append(c)
                 else:
-                    # 浏览器发送http请求
                     self.handle(r)
 
     # 处理客户端具体请求
-    def handle(self,connfd):
-        request = connfd.recv(4096) # 接收http请求
-
-        # 防止客户端退出data等于空
+    def handle(self, connfd):
+        request = connfd.recv(4096)
+        print(request.decode())
         if not request:
-            self.rlist.remove(connfd) # 移除监控
+            self.rlist.remove(connfd)
             connfd.close()
             return
 
         info = request.decode().split(' ')[1]
         print("请求内容：", info)
 
-        # 请求主页
         if info == '/':
             info = '/index.html'
 
         try:
-            f = open(self.dir+info)
+            f = open(self.dir + info)
         except:
             data = "HTTP/1.1 404 Not Found\r\n"
             data += "Content-Type:text/html\r\n"
@@ -68,20 +66,17 @@ class HTTPServer:
             data = "HTTP/1.1 200 OK\r\n"
             data += "Content-Type:text/html\r\n"
             data += "\r\n"
-            data += f.read() # 网页内容
+            data += f.read()
             f.close()
         connfd.send(data.encode())
-
 
 
 if __name__ == '__main__':
     """
     希望通过使用这个类，可以快速搭建一个服务，展示我的网页
     """
-    # 用户自己决定的内容
-    HOST = '0.0.0.0'
+    HOST = '127.0.0.1'
     PORT = 8888
     DIR = "./static"
-    # 实例化对象 --》 对象调用方法实现具体功能
-    httpd = HTTPServer(HOST,PORT,DIR)
-    httpd.serve_forever() # 启动服务
+    httpd = HTTPServer(HOST, PORT, DIR)
+    httpd.serve_forever()
