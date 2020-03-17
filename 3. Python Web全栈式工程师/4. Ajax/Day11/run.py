3a158e47dbeb07563405123721f93efef63530fe
ShaoxiongYuan <58580328+ShaoxiongYuan@users.noreply.github.com>
2020-03-17 16:15:03 +0800

    d11

--- 3. Python Web全栈式工程师/4. Ajax/Day11/run.py
+++ 3. Python Web全栈式工程师/4. Ajax/Day11/run.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -6,6 +6,22 @@ app = Flask(__name__)
 @app.route('/demo')
 def demo_view():
     return render_template('demo01.html')
+
+
+@app.route('/demo2')
+def exer1_view():
+    return render_template('exercise01.html')
+
+
+@app.route('/server')
+def server_view():
+    return '接收前端AJAX请求成功'
+
+
+@app.route('/exer_server')
+def server2():
+    uname = request.args.get('uname')
+    return uname
 
 
 if __name__ == '__main__':
