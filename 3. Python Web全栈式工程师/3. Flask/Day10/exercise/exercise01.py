d7544d21b3868469e917366d60562d51be4d6590
ShaoxiongYuan <58580328+ShaoxiongYuan@users.noreply.github.com>
2020-03-17 10:21:26 +0800

    d11

--- 3. Python Web全栈式工程师/3. Flask/Day10/exercise/exercise01.py
+++ 3. Python Web全栈式工程师/3. Flask/Day10/exercise/exercise01.py
@@ -1,26 +1,21 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 
-@app.route('/all')
-def all():
-    return '所有难度'
-
-
-@app.route('/easy')
-def easy():
-    return '低级难度'
-
-
-@app.route('/middle')
-def middle():
-    return '中级难度'
-
-
-@app.route('/high')
-def high():
-    return '高级难度'
+@app.route('/test')
+def test():
+    level = request.args.get('level')
+    if level == 'all':
+        return '全部难度'
+    elif level == 'easy':
+        return '低级难度'
+    elif level == 'middle':
+        return '中级难度'
+    elif level == 'high':
+        return '高级难度'
+    else:
+        return '获取难度失败'
 
 
 if __name__ == '__main__':
