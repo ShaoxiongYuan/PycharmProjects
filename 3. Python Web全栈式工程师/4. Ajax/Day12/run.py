419e247e1bfe7ba29029ee0f1d02fc18746ca9cd
ShaoxiongYuan <58580328+ShaoxiongYuan@users.noreply.github.com>
2020-03-18 12:00:16 +0800

    d12

--- 3. Python Web全栈式工程师/4. Ajax/Day12/run.py
+++ 3. Python Web全栈式工程师/4. Ajax/Day12/run.py
@@ -3,15 +3,13 @@ from flask import Flask, request, render_template
 app = Flask(__name__)
 
 
-@app.route('/post')
+@app.route('/post', methods=['GET', 'POST'])
 def view1():
-    return render_template('demo01.html')
-
-
-@app.route('/post_server', methods=['POST'])
-def server1():
-    uname = request.form.get('uname')
-    return '欢迎%s' % uname
+    if request.method == 'GET':
+        return render_template('demo01.html')
+    elif request.method == 'POST':
+        uname = request.form.get('uname')
+        return '欢迎%s' % uname
 
 
 if __name__ == '__main__':
