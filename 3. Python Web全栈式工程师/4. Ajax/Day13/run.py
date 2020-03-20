a3c23377ea53e6ca7e1751f6944dcc25f2594349
ShaoxiongYuan <58580328+ShaoxiongYuan@users.noreply.github.com>
2020-03-20 12:01:11 +0800

    d13

--- 3. Python Web全栈式工程师/4. Ajax/Day13/run.py
+++ 3. Python Web全栈式工程师/4. Ajax/Day13/run.py
@@ -3,5 +3,34 @@ import json
 
 app = Flask(__name__)
 
+
+@app.route('/load')
+def load():
+    return render_template('demo01.html')
+
+
+@app.route('/load_server', methods=['GET', 'POST'])
+def load_server():
+    # if request.method == 'GET':
+    #     uname = request.args.get('uname')
+    #     age = request.args.get('age')
+    #     print(uname, age)
+    # elif request.method == 'POST':
+    #     uname = request.form.get('uname')
+    #     age = request.form.get('age')
+    #     print(uname, age)
+    return render_template('demo01_server.html')
+
+
+@app.route('/header')
+def header_view():
+    return render_template('header.html')
+
+
+@app.route('/footer')
+def footer_view():
+    return render_template('footer.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
