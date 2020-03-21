e2559415856c530f1b36ea1452b99c2d1b71cbeb
ShaoxiongYuan <58580328+ShaoxiongYuan@users.noreply.github.com>
2020-03-21 18:01:28 +0800

    d14

--- 3. Python Web全栈式工程师/project_month03/Day14/run.py
+++ 3. Python Web全栈式工程师/project_month03/Day14/run.py
@@ -1,5 +1,4 @@
-from flask import Flask, render_template, request
-import json
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -7,6 +6,11 @@ app = Flask(__name__)
 @app.route('/demo')
 def demo_view():
     return render_template('demo01.html')
+
+
+@app.route('/')
+def index_view():
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
