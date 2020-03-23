dc909fbe44c403c70c8ae176401ce79a9d16d965
ShaoxiongYuan <58580328+ShaoxiongYuan@users.noreply.github.com>
2020-03-23 17:54:57 +0800

    d15

--- 3. Python Web全栈式工程师/project_month03/Day15/run.py
+++ 3. Python Web全栈式工程师/project_month03/Day15/run.py
@@ -1,4 +1,5 @@
 import json
+import time
 
 from flask import Flask, render_template, request
 
@@ -20,7 +21,28 @@ def server():
         else:
             size = int(size)
             data = all_data[size:size + 10]
-    return json.dumps({"code": 200, "data": data})
+    if data:
+        return json.dumps({"code": 200, "data": data})
+    else:
+        return json.dumps({"code": 201, "error": "没有数据了"})
+
+
+@app.route('/add', methods=['POST'])
+def add():
+    name = request.json.get('name')
+    content = request.json.get('content')
+    with open('comment.data', 'r') as f:
+        all_data = json.loads(f.read())
+    with open('comment.data', 'w') as f:
+        new_data = {
+            "num": len(all_data) + 1,
+            "username": name,
+            "time": time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()),
+            "content": content
+        }
+        all_data.append(new_data)
+        f.write(json.dumps(all_data))
+    return json.dumps({"code": 200, "msg": "发表成功"})
 
 
 if __name__ == '__main__':
