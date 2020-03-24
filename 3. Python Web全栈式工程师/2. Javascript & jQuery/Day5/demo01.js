be55df574beea0433ad19b4c6c3786cb27c5f6cd
ShaoxiongYuan <58580328+ShaoxiongYuan@users.noreply.github.com>
2020-03-24 10:51:40 +0800

    d1

--- 3. Python Web全栈式工程师/2. Javascript & jQuery/Day5/demo01.js
+++
@@ -1,1 +0,0 @@
-alert('外部javascript');
--- 3. Python Web全栈式工程师/4. Ajax/Day11/static/demo01.js
+++
@@ -1,7 +0,0 @@
-// if (XMLHttpRequest) {
-//     var xhr = new XMLHttpRequest()
-// } else {
-//     var xhr = new ActiveXObject("Microsoft.XMLHTTP")
-// }
-var xhr = new XMLHttpRequest()
-
