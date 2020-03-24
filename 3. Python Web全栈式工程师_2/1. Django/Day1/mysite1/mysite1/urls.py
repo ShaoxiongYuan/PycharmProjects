b756b3c26bfed353613543090ace1a3f09db498b
ShaoxiongYuan <58580328+ShaoxiongYuan@users.noreply.github.com>
2020-03-24 17:49:36 +0800

    d1

--- 3. Python Web全栈式工程师_2/1. Django/Day1/mysite1/mysite1/urls.py
+++ 3. Python Web全栈式工程师_2/1. Django/Day1/mysite1/mysite1/urls.py
@@ -1,21 +1,16 @@
-"""mysite1 URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
+
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    re_path(r'^$', views.main_view),
+    path('page1', views.page1_view),
+    path('page2', views.page2_view),
+    re_path(r'page(\d+)', views.pagen_view),
+    re_path(r'^(\d+)/(\S+)/(\d+)', views.calculate_view),
+    re_path(r'^person/(?P<name>\w+)/(?P<age>\d{1,2})', views.person_view),
+    re_path(r'^birthday/(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<day>\d{1,2})', views.birth_view),
+    re_path(r'^birthday/(?P<month>\d{1,2})/(?P<day>\d{1,2})/(?P<year>\d{4})', views.birth_view),
 ]
---
+++ 3. Python Web全栈式工程师_2/1. Django/Day1/mysite1/mysite1/views.py
@@ -0,0 +1,55 @@
+from django.http import HttpResponse
+
+
+def main_view(request):
+    return HttpResponse('<h1>这是我的首页</h1>')
+
+
+def page1_view(request):
+    html = "<h1>这是编号为1的页面</h1>"
+    return HttpResponse(html)
+
+
+def page2_view(request):
+    html = "<h1>这是编号为2的页面</h1>"
+    return HttpResponse(html)
+
+
+def pagen_view(request, num):
+    html = "<h1>——————这是编号为" + num + "的页面——————</h1>"
+    return HttpResponse(html)
+
+
+def calculate_view(request, num1, op, num2):
+    num1 = int(num1)
+    num2 = int(num2)
+    if op == 'add':
+        res = num1 + num2
+    elif op == 'sub':
+        res = num1 - num2
+    elif op == 'mul':
+        res = num1 * num2
+    else:
+        return HttpResponse("I am sorry.")
+    return HttpResponse(res)
+
+
+def person_view(request, name, age):
+    return HttpResponse("姓名:%s，年龄:%s" % (name, age))
+
+
+def birth_view(request, year, month, day):
+    print(request.path_info)
+    print(request.get_full_path())
+    print(request.META)
+    print(request.META['REMOTE_ADDR'])
+    print(request.method)
+    # print(request.encoding)
+    # print(request.scheme)
+    # print(request.COOKIES)
+    # print(request.body.decode())
+    # print(request.session)
+    # print(request.environ)
+    # print(request.GET)
+    # print(request.POST)
+    return HttpResponse("生日为：%s年%s月%s日" % (year, month, day))
