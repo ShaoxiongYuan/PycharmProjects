04480d887436ca19238f9b11be0d0678aa335fd2
ShaoxiongYuan <58580328+ShaoxiongYuan@users.noreply.github.com>
2020-03-27 11:20:20 +0800

    d3

--- 3. Python Web全栈式工程师_2/1. Django/Day3/mysite3/mysite3/urls.py
+++ 3. Python Web全栈式工程师_2/1. Django/Day3/mysite3/mysite3/urls.py
@@ -5,5 +5,12 @@ from . import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^test$', views.test)
+    url(r'^test$', views.test),
+    url(r'^base$', views.index, name='base'),
+    url(r'^music$', views.test_music),
+    url(r'^sport$', views.test_sport),
+    url(r'^page1$', views.page1_view, name='page1'),
+    url(r'^page2$', views.page2_view, name='page2'),
+    # url(r'^page(\d+)', views.pagen_view, name='pn'),
+    url(r'^page(?P<n>\d+)', views.pagen_view, name='pn'),
 ]
--- 3. Python Web全栈式工程师_2/1. Django/Day3/mysite3/mysite3/views.py
+++ 3. Python Web全栈式工程师_2/1. Django/Day3/mysite3/mysite3/views.py
@@ -1,9 +1,42 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.urls import reverse
 
 
 def test(request):
     dict1 = {
         "username": "steven",
-        "age": 18
+        "age": 18,
+        "note": "我叫笑笑笑哈哈哈哈"
     }
     return render(request, 'test.html', dict1)
+
+
+def index(request):
+    dict1 = {
+        "lst": ["Tom", "Jack"]
+    }
+    return render(request, 'base.html', dict1)
+
+
+def test_music(request):
+    return render(request, 'music.html')
+
+
+def test_sport(request):
+    return render(request, 'sport.html')
+
+
+def page1_view(request):
+    return render(request, 'page1.html')
+
+
+def page2_view(request):
+    return HttpResponse('我是page2')
+
+
+def pagen_view(request, n):
+    print(reverse('page2'))
+    # print(reverse('pn', args=['500']))
+    print(reverse('pn', kwargs={'n': 500}))
+    return HttpResponse('我是page%s' % n)
