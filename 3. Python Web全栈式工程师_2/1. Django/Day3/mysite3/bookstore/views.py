c5d70217bacd6b6c05e6d7a416a8ed927fc0f6c0
ShaoxiongYuan <58580328+ShaoxiongYuan@users.noreply.github.com>
2020-03-28 17:18:22 +0800

    d4

--- 3. Python Web全栈式工程师_2/1. Django/Day3/mysite3/bookstore/views.py
+++ 3. Python Web全栈式工程师_2/1. Django/Day3/mysite3/bookstore/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 from .models import Book
@@ -6,7 +7,57 @@ from .models import Book
 # Create your views here.
 def book(request):
     books = Book.objects.all()
-    dict1 = {
-        "books": books
-    }
-    return render(request, 'bookstore/book.html', dict1)
+    return render(request, 'bookstore/book.html', locals())
+
+
+def update(request, id):
+    if request.method == 'GET':
+        try:
+            book = Book.objects.get(id=int(id))
+        except Exception as e:
+            print(e)
+            return HttpResponse("No book...")
+        else:
+            return render(request, 'bookstore/update_book.html', locals())
+    elif request.method == 'POST':
+        price = request.POST.get('price')
+        market_price = request.POST.get('market_price')
+
+        if not price or not market_price:
+            return HttpResponse("Please fill in the blanks")
+
+        book = Book.objects.get(id=int(id))
+        if float(price) != book.price or float(market_price) != book.market_price:
+            book.price = price
+            book.market_price = market_price
+            book.save()
+
+        return HttpResponseRedirect('/bookstore/book')
+
+
+def delete(request, id):
+    if request.method == 'GET':
+        try:
+            book = Book.objects.get(id=int(id))
+        except Exception as e:
+            print(e)
+            return HttpResponse("No book...")
+        else:
+            book.delete()
+            return HttpResponseRedirect('/bookstore/book')
+
+
+def add(request):
+    if request.method == 'GET':
+        return render(request, 'bookstore/add_book.html')
+    elif request.method == 'POST':
+        title = request.POST.get('title')
+        pub = request.POST.get('pub')
+        price = request.POST.get('price')
+        market_price = request.POST.get('market_price')
+        books = Book.objects.all()
+        for book in books:
+            if title == book.title:
+                return HttpResponse('书籍已存在')
+        Book.objects.create(title=title, pub=pub, price=price, market_price=market_price)
+        return HttpResponseRedirect('/bookstore/book')
