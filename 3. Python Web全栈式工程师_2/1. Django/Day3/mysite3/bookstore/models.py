31fcabf0bf2fc2d1f9c878f5c148cb24942cef8c
ShaoxiongYuan <58580328+ShaoxiongYuan@users.noreply.github.com>
2020-03-28 11:14:43 +0800

    d4

--- 3. Python Web全栈式工程师_2/1. Django/Day3/mysite3/bookstore/models.py
+++ 3. Python Web全栈式工程师_2/1. Django/Day3/mysite3/bookstore/models.py
@@ -3,6 +3,13 @@ from django.db import models
 
 # Create your models here.
 class Book(models.Model):
-    title = models.CharField("书名", max_length=50, default='')
+    title = models.CharField("书名", max_length=50, default='', unique=True)
+    pub = models.CharField("出版社", max_length=200, default='')
     price = models.DecimalField("定价", max_digits=7, decimal_places=2, default=0.0)
-    desc = models.CharField("描述", max_length=100, default='')
+    market_price = models.DecimalField("零售价", max_digits=7, decimal_places=2, default=0.0)
+
+
+class Author(models.Model):
+    name = models.CharField("姓名", max_length=50, default='')
+    age = models.IntegerField("年龄", default=1)
+    email = models.EmailField("邮箱", null=True)
