90ea40356b20b0d1f7aaffaa8e38957057a47a88
ShaoxiongYuan <58580328+ShaoxiongYuan@users.noreply.github.com>
2020-03-30 15:46:38 +0800

    d5

--- 3. Python Web全栈式工程师_2/1. Django/Day5/mysite3/bookstore/admin.py
+++ 3. Python Web全栈式工程师_2/1. Django/Day5/mysite3/bookstore/admin.py
@@ -1,5 +1,22 @@
-# Register your models here.
 from .models import *
 from django.contrib import admin
 
-admin.site.register(Book)
+
+# Register your models here.
+class BookManager(admin.ModelAdmin):
+    list_display = ['id', 'title', 'pub', 'price', 'market_price']
+    list_display_links = ['title']
+    list_filter = ['pub']
+    list_editable = ['price', 'market_price']
+    search_fields = ['title']
+
+
+admin.site.register(Book, BookManager)
+
+
+class AuthorManager(admin.ModelAdmin):
+    list_display = ['name', 'age', 'email']
+    search_fields = ['name']
+
+
+admin.site.register(Author, AuthorManager)
--- 3. Python Web全栈式工程师_2/1. Django/Day5/mysite3/bookstore/models.py
+++ 3. Python Web全栈式工程师_2/1. Django/Day5/mysite3/bookstore/models.py
@@ -9,6 +9,11 @@ class Book(models.Model):
     market_price = models.DecimalField("零售价", max_digits=7, decimal_places=2, default=0.0)
     is_active = models.BooleanField("是否活跃", default=True)
 
+    class Meta:
+        db_table = 'book'
+        verbose_name = '图书'
+        verbose_name_plural = verbose_name
+
     def __str__(self):
         return "书名: %s, 出版社: %s, 定价: %s, 零售价: %s" % (self.title, self.pub, self.price, self.market_price)
 
@@ -17,6 +22,11 @@ class Author(models.Model):
     name = models.CharField("姓名", max_length=50, default='')
     age = models.IntegerField("年龄", default=1)
     email = models.EmailField("邮箱", null=True)
+
+    class Meta:
+        db_table = 'author'
+        verbose_name = '作者'
+        verbose_name_plural = verbose_name
 
     def __str__(self):
         return "姓名: %s, 年龄: %s, 邮箱: %s" % (self.name, self.age, self.email)
