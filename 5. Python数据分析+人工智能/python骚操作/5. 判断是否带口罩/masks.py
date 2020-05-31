7ecea8327bec19b8da7aa8fe4bf266196329b10b
ShaoxiongYuan <stevenpp123@sina.com>
2020-05-31 10:49:30 +0800

    d8

--- 5. Python数据分析+人工智能/python骚操作/5. 判断是否带口罩/masks.py
+++
@@ -1,9 +0,0 @@
-import paddlehub as hub
-
-module = hub.Module(name="pyramidbox_lite_mobile_mask")
-input_dict = {"image": ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg']}
-
-# 口罩检测预测
-results = module.face_detection(data=input_dict, use_multi_scale=True, shrink=0.6, )
-for result in results:
-    print(result)
