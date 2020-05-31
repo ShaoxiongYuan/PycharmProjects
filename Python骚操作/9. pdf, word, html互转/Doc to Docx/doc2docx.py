be6458a4a7aaa853edaf41968b234351960761ee
ShaoxiongYuan <stevenpp123@sina.com>
2020-05-31 23:07:42 +0800

    d8

--- Python骚操作/9. pdf, word, html互转/Doc to Docx/doc2docx.py
+++ Python骚操作/9. pdf, word, html互转/Doc to Docx/doc2docx.py
@@ -2,14 +2,11 @@ from win32com import client
 
 
 def doc2docx(doc_name, docx_name):
-    try:
-        word = client.DispatchEx('Word.Application')
-        doc = word.Documents.Open(doc_name)
-        doc.SaveAs(docx_name, 16)
-        doc.Close()
-        word.Quit()
-    except:
-        pass
+    word = client.DispatchEx('Word.Application')
+    doc = word.Documents.Open(doc_name)
+    doc.SaveAs(docx_name, 16)
+    doc.Close()
+    word.Quit()
 
 
 if __name__ == '__main__':
--- Python骚操作/9. pdf, word, html互转/Word to PDF/word2pdf.py
+++ Python骚操作/9. pdf, word, html互转/Word to PDF/word2pdf.py
@@ -3,19 +3,16 @@ from win32com import client
 
 
 def doc2pdf(doc_name, pdf_name):
-    try:
-        word = client.Dispatch('Word.Application')
-        if os.path.exists(pdf_name):
-            os.remove(pdf_name)
-        worddoc = word.Documents.Open(doc_name, ReadOnly=1)
-        worddoc.SaveAs(pdf_name, FileFormat=17)
-        worddoc.Close()
-        return pdf_name
-    except:
-        return 1
+    word = client.Dispatch('Word.Application')
+    if os.path.exists(pdf_name):
+        os.remove(pdf_name)
+    worddoc = word.Documents.Open(doc_name, ReadOnly=1)
+    worddoc.SaveAs(pdf_name, FileFormat=17)
+    worddoc.Close()
+    return pdf_name
 
 
 if __name__ == '__main__':
-    doc_name = 'letter.doc'
-    pdf_name = 'result.pdf'
+    doc_name = './letter.doc'
+    pdf_name = './result.pdf'
     print(doc2pdf(doc_name, pdf_name))
