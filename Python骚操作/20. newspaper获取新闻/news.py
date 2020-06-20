f652e4db89a594119f3521c16105338179f50489
ShaoxiongYuan <stevenpp123@sina.com>
2020-06-20 11:44:46 +0800

    d14

---
+++ Python骚操作/20. newspaper获取新闻/news.py
@@ -0,0 +1,19 @@
+from newspaper import *
+
+url = 'http://fox13now.com/2013/12/30/new-year-new-laws-obamacare-pot-guns-and-drones/'
+article = Article(url)
+
+article.download()
+# print(article.html)
+#
+article.parse()
+# print(article.authors)
+# print(article.publish_date)
+# print(article.text)
+# print(article.top_image)
+# print(article.movies)
+
+article.nlp()
+print(article.keywords)
+print(article.summary)
+
