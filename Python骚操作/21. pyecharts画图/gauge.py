5b05f29cea7b34cd654351dbc4245696bb1aa90f
ShaoxiongYuan <2379773801@qq.com>
2020-11-29 18:43:11 +0800

    update

--- Python骚操作/21. pyecharts画图/gauge.py
+++
@@ -1,8 +0,0 @@
-from pyecharts import charts
-
-# 仪表盘
-gauge = charts.Gauge()
-gauge.add('Python小例子', [('Python机器学习', 30), ('Python基础', 70.),
-                        ('Python正则', 90)])
-gauge.render(path="./data/仪表盘.html")
-print('ok')
